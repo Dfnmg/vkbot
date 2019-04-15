897d53df65633976f16d44ee4b4832b8ce2dfb75
Dfnmg <48044028+Dfnmg@users.noreply.github.com>
2019-04-15 23:30:18 +0500

    Vkbot.py

---
+++ Vkbot.py
@@ -0,0 +1,22 @@
+import requests
+import vk_api
+
+vk_session = vk_api.VkApi(token='токен с доступом к сообщениям и фото')
+
+from vk_api.longpoll import VkLongPoll, VkEventType
+longpoll = VkLongPoll(vk_session)
+vk = vk_session.get_api()
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+   #Слушаем longpoll, если пришло сообщение то:			
+        if event.text == 'Первый вариант фразы' or event.text == 'Второй вариант фразы': #Если написали заданную фразу
+            if event.from_user: #Если написали в ЛС
+                vk.messages.send( #Отправляем сообщение
+                    user_id=event.user_id,
+                    message='Ваш текст'
+		)
+            elif event.from_chat: #Если написали в Беседе
+                vk.messages.send( #Отправляем собщение
+                    chat_id=event.chat_id,
+                    message='Ваш текст'
+		)
